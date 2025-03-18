87f31964b0fe470a10cd25f91b516eeed6911e97
evandrosg <evandrosg@gmail.com>
2025-03-18 16:27:35 -0300

    aa

--- inserir_dados.php
+++ inserir_dados.php
@@ -1,17 +1,8 @@
 <?php
-// Defina uma chave API segura (crie uma aleatória)
-$api_key = "MinhaChaveSuperSecreta123";
-
-// Verifica se a chave enviada é válida
-if (!isset($_POST['api_key']) || $_POST['api_key'] !== $api_key) {
-    die("Acesso negado: Chave API inválida.");
-}
-
-// Conectar ao banco de dados
 $server = "esp32server.database.windows.net";
 $user = "evandro";
 $password = "Senai@106";
-$db = "esp32server/SQL_Evandro";
+$db = "esp32server/Senai@106";
 
 $conn = new mysqli($server, $user, $password, $db);
 
@@ -19,11 +10,9 @@ if ($conn->connect_error) {
     die("Falha na conexão: " . $conn->connect_error);
 }
 
-// Captura os dados enviados pela ESP32
 $temperatura = $_POST['temperature'];
 $umidade = $_POST['humidity'];
 
-// Insere os dados no banco
 $sql = "INSERT INTO DHT22_Dados (temperatura, umidade) VALUES ('$temperatura', '$umidade')";
 
 if ($conn->query($sql) === TRUE) {
