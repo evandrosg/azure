9a50efa2d290325cce5dd03e7368dc4452a7a120
evandrosg <evandrosg@gmail.com>
2025-03-18 15:46:19 -0300

    aa

---
+++ inserir_dados.php
@@ -0,0 +1,25 @@
+<?php
+$server = "sp32server.database.windows.net";
+$user = "evandro";
+$password = "sua_senha";
+$db = "Senai@106";
+
+$conn = new mysqli($server, $user, $password, $db);
+
+if ($conn->connect_error) {
+    die("Falha na conexão: " . $conn->connect_error);
+}
+
+$temperatura = $_POST['temperature'];
+$umidade = $_POST['humidity'];
+
+$sql = "INSERT INTO DHT22_Dados (temperatura, umidade) VALUES ('$temperatura', '$umidade')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Dados salvos com sucesso!";
+} else {
+    echo "Erro: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?>
